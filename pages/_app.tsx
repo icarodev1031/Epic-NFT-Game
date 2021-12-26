dae8e614830e3bd087c5ed1b20993bf50a92a871
Viral Sangani <viralsangani@Viral-Sangani-B479s-MacBook-Pro.local>
2021-12-26 12:50:49 +0530

    basic wallet functions added using ethers.js ✨

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import type { AppProps } from "next/app";
+import { DappProvider } from "../contexts/DappContext";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <DappProvider>
+      <Component {...pageProps} />;
+    </DappProvider>
+  );
 }
 
 export default MyApp;
