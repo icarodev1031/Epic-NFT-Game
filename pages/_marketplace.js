c809f94df8be3399b086f6437cd2e2d305cb7aa0
icarodev <icarodev1031@gmail.com>
2022-08-26 11:24:39 +0900

    _marketplace.js update

--- pages/_marketplace.js
+++ pages/_marketplace.js
@@ -1,7 +1,6 @@
 
-import { useEffect, useState } from 'react'
 import { ethers } from 'ethers'
-import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 import Web3Modal from 'web3modal'
 
@@ -11,58 +10,81 @@ marketplaceAddress
 
 import NFTMarketplace from '../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json'
 
-export default function ResellNFT() {
-const [formInput, updateFormInput] = useState({ price: '', image: '' })
-const router = useRouter()
-const { id, tokenURI } = router.query
-const { image, price } = formInput
-
+export default function Home() {
+const [nfts, setNfts] = useState([])
+const [loadingState, setLoadingState] = useState('not-loaded')
 useEffect(() => {
-    fetchNFT()
-}, [id])
+    loadNFTs()
+}, [])
+async function loadNFTs() {
+    /* create a generic provider and query for unsold market items */
+    const provider = new ethers.providers.JsonRpcProvider()
+    const contract = new ethers.Contract(marketplaceAddress, NFTMarketplace.abi, provider)
+    const data = await contract.fetchMarketItems()
 
-async function fetchNFT() {
-    if (!tokenURI) return
-    const meta = await axios.get(tokenURI)
-    updateFormInput(state => ({ ...state, image: meta.data.image }))
+    /*
+    *  map over items returned from smart contract and format 
+    *  them as well as fetch their token metadata
+    */
+    const items = await Promise.all(data.map(async i => {
+    const tokenUri = await contract.tokenURI(i.tokenId)
+    const meta = await axios.get(tokenUri)
+    let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
+    let item = {
+        price,
+        tokenId: i.tokenId.toNumber(),
+        seller: i.seller,
+        owner: i.owner,
+        image: meta.data.image,
+        name: meta.data.name,
+        description: meta.data.description,
+    }
+    return item
+    }))
+    setNfts(items)
+    setLoadingState('loaded')
 }
-
-async function listNFTForSale() {
-    if (!price) return
+async function buyNft(nft) {
+    /* needs the user to sign the transaction, so will use Web3Provider and sign it */
     const web3Modal = new Web3Modal()
     const connection = await web3Modal.connect()
     const provider = new ethers.providers.Web3Provider(connection)
     const signer = provider.getSigner()
+    const contract = new ethers.Contract(marketplaceAddress, NFTMarketplace.abi, signer)
 
-    const priceFormatted = ethers.utils.parseUnits(formInput.price, 'ether')
-    let contract = new ethers.Contract(marketplaceAddress, NFTMarketplace.abi, signer)
-    let listingPrice = await contract.getListingPrice()
-
-    listingPrice = listingPrice.toString()
-    let transaction = await contract.resellToken(id, priceFormatted, { value: listingPrice })
+    /* user will be prompted to pay the asking proces to complete the transaction */
+    const price = ethers.utils.parseUnits(nft.price.toString(), 'ether')
+    const transaction = await contract.createMarketSale(nft.tokenId, {
+    value: price
+    })
     await transaction.wait()
-
-    router.push('/')
+    loadNFTs()
 }
-
+if (loadingState === 'loaded' && !nfts.length) return (<h1 className="px-20 py-10 text-3xl">No items in marketplace</h1>)
 return (
     <div className="flex justify-center">
-    <div className="w-1/2 flex flex-col pb-12">
-        <input
-        placeholder="Asset Price in Eth"
-        className="mt-2 border rounded p-4"
-        onChange={e => updateFormInput({ ...formInput, price: e.target.value })}
-        />
+    <div className="px-4" style={{ maxWidth: '1600px' }}>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
         {
-        image && (
-            <img className="rounded mt-4" width="350" src={image} />
-        )
+            nfts.map((nft, i) => (
+            <div key={i} className="border shadow rounded-xl overflow-hidden">
+                <img src={nft.image} />
+                <div className="p-4">
+                <p style={{ height: '64px' }} className="text-2xl font-semibold">{nft.name}</p>
+                <div style={{ height: '70px', overflow: 'hidden' }}>
+                    <p className="text-gray-400">{nft.description}</p>
+                </div>
+                </div>
+                <div className="p-4 bg-black">
+                <p className="text-2xl font-bold text-white">{nft.price} ETH</p>
+                <button className="mt-4 w-full bg-pink-500 text-white font-bold py-2 px-12 rounded" onClick={() => buyNft(nft)}>Buy</button>
+                </div>
+            </div>
+            ))
         }
-        <button onClick={listNFTForSale} className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg">
-        List NFT
-        </button>
+        </div>
     </div>
     </div>
 )
 }
-    2022-08-26 11:24:38.090402
+    2022-08-26 11:24:38.928287
