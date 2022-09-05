211c86a9fd0a0f5145aefc27288c342c37d446de
icarodev <icarodev1031@gmail.com>
2022-09-05 18:53:10 +0800

    _howtoplay.js update

--- pages/_howtoplay.js
+++ pages/_howtoplay.js
@@ -1,37 +1,68 @@
 
-import '../styles/globals.css'
-import Link from 'next/link'
-function MyApp({ Component, pageProps }) {
-return (
-    <div>
-    <nav className='border-b p-6'>
-    <p className="text-4xl font-bold">Metaverse Marketplace</p>
-        <div className="flex mt-4">
-        <Link href="/">
-            <a className="mr-4 text-pink-500">
-            Home
-            </a>
-        </Link>
-        <Link href="/create-nft">
-            <a className="mr-6 text-pink-500">
-            Sell NFT
-            </a>
-        </Link>
-        <Link href="/my-nfts">
-            <a className="mr-6 text-pink-500">
-            My NFTs
-            </a>
-        </Link>
-        <Link href="/dashboard">
-            <a className="mr-6 text-pink-500">
-            Dashboard
-            </a>
-        </Link>
-        </div>
-    </nav>
-    <Component {...pageProps}/>
-    </div>
-    )
+import { useEffect, useState } from 'react'
+import { ethers } from 'ethers'
+import { useRouter } from 'next/router'
+import axios from 'axios'
+import Web3Modal from 'web3modal'
+
+import {
+marketplaceAddress
+} from '../config'
+
+import NFTMarketplace from '../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json'
+
+export default function ResellNFT() {
+const [formInput, updateFormInput] = useState({ price: '', image: '' })
+const router = useRouter()
+const { id, tokenURI } = router.query
+const { image, price } = formInput
+
+useEffect(() => {
+    fetchNFT()
+}, [id])
+
+async function fetchNFT() {
+    if (!tokenURI) return
+    const meta = await axios.get(tokenURI)
+    updateFormInput(state => ({ ...state, image: meta.data.image }))
 }
-export default MyApp
-    2022-09-05 18:53:09.491138
+
+async function listNFTForSale() {
+    if (!price) return
+    const web3Modal = new Web3Modal()
+    const connection = await web3Modal.connect()
+    const provider = new ethers.providers.Web3Provider(connection)
+    const signer = provider.getSigner()
+
+    const priceFormatted = ethers.utils.parseUnits(formInput.price, 'ether')
+    let contract = new ethers.Contract(marketplaceAddress, NFTMarketplace.abi, signer)
+    let listingPrice = await contract.getListingPrice()
+
+    listingPrice = listingPrice.toString()
+    let transaction = await contract.resellToken(id, priceFormatted, { value: listingPrice })
+    await transaction.wait()
+
+    router.push('/')
+}
+
+return (
+    <div className="flex justify-center">
+    <div className="w-1/2 flex flex-col pb-12">
+        <input
+        placeholder="Asset Price in Eth"
+        className="mt-2 border rounded p-4"
+        onChange={e => updateFormInput({ ...formInput, price: e.target.value })}
+        />
+        {
+        image && (
+            <img className="rounded mt-4" width="350" src={image} />
+        )
+        }
+        <button onClick={listNFTForSale} className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg">
+        List NFT
+        </button>
+    </div>
+    </div>
+)
+}
+    2022-09-05 18:53:09.971846
