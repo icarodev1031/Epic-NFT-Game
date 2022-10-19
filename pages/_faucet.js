49e71dfe1660eae7d8e4bdf2731be5d68f92fb41
icaro <icarodev1031@gmail.com>
2022-10-19 16:53:35 -0700

    _faucet.js update

--- pages/_faucet.js
+++ pages/_faucet.js
@@ -1,90 +1,37 @@
 
-import { ethers } from 'ethers'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import Web3Modal from 'web3modal'
-
-import {
-marketplaceAddress
-} from '../config'
-
-import NFTMarketplace from '../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json'
-
-export default function Home() {
-const [nfts, setNfts] = useState([])
-const [loadingState, setLoadingState] = useState('not-loaded')
-useEffect(() => {
-    loadNFTs()
-}, [])
-async function loadNFTs() {
-    /* create a generic provider and query for unsold market items */
-    const provider = new ethers.providers.JsonRpcProvider()
-    const contract = new ethers.Contract(marketplaceAddress, NFTMarketplace.abi, provider)
-    const data = await contract.fetchMarketItems()
-
-    /*
-    *  map over items returned from smart contract and format 
-    *  them as well as fetch their token metadata
-    */
-    const items = await Promise.all(data.map(async i => {
-    const tokenUri = await contract.tokenURI(i.tokenId)
-    const meta = await axios.get(tokenUri)
-    let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
-    let item = {
-        price,
-        tokenId: i.tokenId.toNumber(),
-        seller: i.seller,
-        owner: i.owner,
-        image: meta.data.image,
-        name: meta.data.name,
-        description: meta.data.description,
-    }
-    return item
-    }))
-    setNfts(items)
-    setLoadingState('loaded')
-}
-async function buyNft(nft) {
-    /* needs the user to sign the transaction, so will use Web3Provider and sign it */
-    const web3Modal = new Web3Modal()
-    const connection = await web3Modal.connect()
-    const provider = new ethers.providers.Web3Provider(connection)
-    const signer = provider.getSigner()
-    const contract = new ethers.Contract(marketplaceAddress, NFTMarketplace.abi, signer)
-
-    /* user will be prompted to pay the asking proces to complete the transaction */
-    const price = ethers.utils.parseUnits(nft.price.toString(), 'ether')
-    const transaction = await contract.createMarketSale(nft.tokenId, {
-    value: price
-    })
-    await transaction.wait()
-    loadNFTs()
-}
-if (loadingState === 'loaded' && !nfts.length) return (<h1 className="px-20 py-10 text-3xl">No items in marketplace</h1>)
+import '../styles/globals.css'
+import Link from 'next/link'
+function MyApp({ Component, pageProps }) {
 return (
-    <div className="flex justify-center">
-    <div className="px-4" style={{ maxWidth: '1600px' }}>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
-        {
-            nfts.map((nft, i) => (
-            <div key={i} className="border shadow rounded-xl overflow-hidden">
-                <img src={nft.image} />
-                <div className="p-4">
-                <p style={{ height: '64px' }} className="text-2xl font-semibold">{nft.name}</p>
-                <div style={{ height: '70px', overflow: 'hidden' }}>
-                    <p className="text-gray-400">{nft.description}</p>
-                </div>
-                </div>
-                <div className="p-4 bg-black">
-                <p className="text-2xl font-bold text-white">{nft.price} ETH</p>
-                <button className="mt-4 w-full bg-pink-500 text-white font-bold py-2 px-12 rounded" onClick={() => buyNft(nft)}>Buy</button>
-                </div>
-            </div>
-            ))
-        }
+    <div>
+    <nav className='border-b p-6'>
+    <p className="text-4xl font-bold">Metaverse Marketplace</p>
+        <div className="flex mt-4">
+        <Link href="/">
+            <a className="mr-4 text-pink-500">
+            Home
+            </a>
+        </Link>
+        <Link href="/create-nft">
+            <a className="mr-6 text-pink-500">
+            Sell NFT
+            </a>
+        </Link>
+        <Link href="/my-nfts">
+            <a className="mr-6 text-pink-500">
+            My NFTs
+            </a>
+        </Link>
+        <Link href="/dashboard">
+            <a className="mr-6 text-pink-500">
+            Dashboard
+            </a>
+        </Link>
         </div>
+    </nav>
+    <Component {...pageProps}/>
     </div>
-    </div>
-)
+    )
 }
-    2022-10-13 19:51:28.736076
+export default MyApp
+    2022-10-19 16:53:34.870085
