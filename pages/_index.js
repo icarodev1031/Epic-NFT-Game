e0d79942ed50991a651d95a3eb5a8d75383a40ff
icarodev1031 <icarodev1031@gmail.com>
2022-11-22 14:35:05 -0800

    _index.js update

--- pages/_index.js
+++ pages/_index.js
@@ -1,7 +1,6 @@
 
-import { useEffect, useState } from 'react'
 import { ethers } from 'ethers'
-import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 import Web3Modal from 'web3modal'
 
@@ -11,58 +10,60 @@ marketplaceAddress
 
 import NFTMarketplace from '../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json'
 
-export default function ResellNFT() {
-const [formInput, updateFormInput] = useState({ price: '', image: '' })
-const router = useRouter()
-const { id, tokenURI } = router.query
-const { image, price } = formInput
-
+export default function CreatorDashboard() {
+const [nfts, setNfts] = useState([])
+const [loadingState, setLoadingState] = useState('not-loaded')
 useEffect(() => {
-    fetchNFT()
-}, [id])
-
-async function fetchNFT() {
-    if (!tokenURI) return
-    const meta = await axios.get(tokenURI)
-    updateFormInput(state => ({ ...state, image: meta.data.image }))
-}
-
-async function listNFTForSale() {
-    if (!price) return
-    const web3Modal = new Web3Modal()
+    loadNFTs()
+}, [])
+async function loadNFTs() {
+    const web3Modal = new Web3Modal({
+    network: 'mainnet',
+    cacheProvider: true,
+    })
     const connection = await web3Modal.connect()
     const provider = new ethers.providers.Web3Provider(connection)
     const signer = provider.getSigner()
 
-    const priceFormatted = ethers.utils.parseUnits(formInput.price, 'ether')
-    let contract = new ethers.Contract(marketplaceAddress, NFTMarketplace.abi, signer)
-    let listingPrice = await contract.getListingPrice()
+    const contract = new ethers.Contract(marketplaceAddress, NFTMarketplace.abi, signer)
+    const data = await contract.fetchItemsListed()
 
-    listingPrice = listingPrice.toString()
-    let transaction = await contract.resellToken(id, priceFormatted, { value: listingPrice })
-    await transaction.wait()
+    const items = await Promise.all(data.map(async i => {
+    const tokenUri = await contract.tokenURI(i.tokenId)
+    const meta = await axios.get(tokenUri)
+    let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
+    let item = {
+        price,
+        tokenId: i.tokenId.toNumber(),
+        seller: i.seller,
+        owner: i.owner,
+        image: meta.data.image,
+    }
+    return item
+    }))
 
-    router.push('/')
+    setNfts(items)
+    setLoadingState('loaded') 
 }
-
+if (loadingState === 'loaded' && !nfts.length) return (<h1 className="py-10 px-20 text-3xl">No NFTs listed</h1>)
 return (
-    <div className="flex justify-center">
-    <div className="w-1/2 flex flex-col pb-12">
-        <input
-        placeholder="Asset Price in Eth"
-        className="mt-2 border rounded p-4"
-        onChange={e => updateFormInput({ ...formInput, price: e.target.value })}
-        />
+    <div>
+    <div className="p-4">
+        <h2 className="text-2xl py-2">Items Listed</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
         {
-        image && (
-            <img className="rounded mt-4" width="350" src={image} />
-        )
+            nfts.map((nft, i) => (
+            <div key={i} className="border shadow rounded-xl overflow-hidden">
+                <img src={nft.image} className="rounded" />
+                <div className="p-4 bg-black">
+                <p className="text-2xl font-bold text-white">Price - {nft.price} Eth</p>
+                </div>
+            </div>
+            ))
         }
-        <button onClick={listNFTForSale} className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg">
-        List NFT
-        </button>
+        </div>
     </div>
     </div>
 )
 }
-    2022-11-22 14:34:28.264950
+    2022-11-22 14:34:49.492508
