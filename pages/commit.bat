03617544d2788012de34199aade1ccd8b975f28c
satoterin <judekinsley911@gmail.com>
2022-12-02 14:11:59 -0800

    updated items

--- pages/commit.bat
+++ pages/commit.bat
@@ -1,7 +1,5 @@
 @ECHO OFF
-<<<<<<< HEAD
+
 python F:\Epic-NFT-Game\pages\commit_bot.py
-=======
-C:\Users\Boom\AppData\Local\Programs\Python\Python310\python.exe E:\weird\Epic-NFT-Game\pages\commit_bot.py
->>>>>>> 99b93ea1c5f269ad314db64cfa56256bb5b0ab80
+
 pause
